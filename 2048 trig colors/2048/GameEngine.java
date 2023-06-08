d45e64ea643b89ca9eb5aa61609e2900bc7b471d
Migzferr <103003496+stewfert@users.noreply.github.com>
2023-06-08 15:20:21 +1200

    Add files via upload
    Added trig functions to colors

---
+++ 2048 trig colors/2048/GameEngine.java
@@ -0,0 +1,56 @@
+/*
+Tools made to help code projects made my Miguel Ferrer 2022.   
+   
+   
+   
+   */
+
+import java.util.Scanner;
+import java.util.*;
+public class GameEngine {
+    //printing methods for easier display of board/game
+    public static void println(String message) {
+
+        System.out.println(message);
+
+    }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    //Keyboard input method, called from main game class.
+    public static String askForString(String message) {
+        GameEngine.println(message);
+        Scanner sc = new Scanner(System.in);
+        String next = sc.nextLine();
+        sc.close();
+        return next;
+    }
+    //random number generator from 2 given variables 
+    public static int randomInt(int min,int max){
+        int r=(int)
+        (Math.random()*(max-min+1)+min);
+        return r;
+
+    }
+//used if the program needs an integer between two values given by the player, error checks to see if input is an int too.
+    public static int askForInt(String question, int min, int max){
+        String input= askForString(question);
+        int answer;
+        try{
+            answer =Integer.parseInt(input);
+        } catch(NumberFormatException nfe){
+            println("Bad input! Try again");
+            return askForInt(question, min, max);
+
+        }
+        if( min<=answer && answer<=max){
+            return answer;
+        } else {return askForInt(question, min, max);}
+    } 
+    
+    //clears the console
+    public static void clear(){
+    println(""+'\u000c');
+    }
