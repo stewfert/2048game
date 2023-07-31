10fa93d8ad9bc17560fd1d49bf3291f570319766
Migzferr <103003496+stewfert@users.noreply.github.com>
2023-07-31 20:01:30 +1200

    Add files via upload
    Fixed flickering so that I don't update the entire JPanel each time I repaint but the JFrame. Also migrated most of the GUI code to a separate class.

---
+++ GUI V6 flicker FIXED/Main.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    Game game = new Game();
+   private  final int PANEL_X_SIZE=400, PANEL_Y_SIZE=400;
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+                    new Main();
+            });
+    }
+
+    public Main() {
+        setTitle("2048");
+       
+        this.getContentPane().setPreferredSize(new Dimension(PANEL_X_SIZE, PANEL_Y_SIZE));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.add(game, BorderLayout.CENTER);
+        this.setSize(PANEL_X_SIZE, PANEL_Y_SIZE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.requestFocus();
+        
+        //center the new JPanel on the screen
+        
+
+        // Call the gameMethod() in the Game class to set up the game board and initialize the initial state.
+        game.gameMethod();
+
+        // Add the key listener to the Game class, not the JPanel class.
+        this.addKeyListener(game);
+        this.setFocusable(true);
+        this.requestFocusInWindow();
+    }
+    
+    
+}
